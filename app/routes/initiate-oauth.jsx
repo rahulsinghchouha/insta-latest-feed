92584b58b68e8df818eed0928518c86a0ae8b610
Rahul Singh Chouhan <134701207+rahulsinghchouha@users.noreply.github.com>
2024-09-25 16:11:09 +0530

    console log added initiate-oauth.jsx

--- app/routes/initiate-oauth.jsx
+++ app/routes/initiate-oauth.jsx
@@ -15,6 +15,7 @@ export const action = async ({ request }) => {
 	}
 
   try {
+    console.log("\n Redirecting user to Instagram OAuth \n");
     return redirect("https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=488937830712253&=https://admin.shopify.com/store/citsapptesting/apps/cits-instafeed-1/user-oauth&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish");
   } catch (error) {
     console.error(error);
