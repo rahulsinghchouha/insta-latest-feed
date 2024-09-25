f8672a787d090b24e745dc094d15a39209b4d669
Rahul Singh Chouhan <134701207+rahulsinghchouha@users.noreply.github.com>
2024-09-25 15:54:27 +0530

    Update IndexComponent.jsx

--- app/components/IndexComponent.jsx
+++ app/components/IndexComponent.jsx
@@ -31,7 +31,10 @@ function IndexComponent({
 
   function handleInstagramLink() {
     try{
-      redirect.dispatch(Redirect.Action.REMOTE, 'https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=488937830712253&redirect_uri=https://instacarousel24.onrender.com/user-oauth&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish');
+      fetcher.submit({},{
+				method:"POST",
+				action:"/app"
+			})
     } catch(err){
       console.log(err);
     }
