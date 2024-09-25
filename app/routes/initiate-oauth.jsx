d5a84a8d35e52ec574866dfb13d61598f60f917c
Rahul Singh Chouhan <134701207+rahulsinghchouha@users.noreply.github.com>
2024-09-25 16:50:03 +0530

    target _parent initiate-oauth.jsx

--- app/routes/initiate-oauth.jsx
+++ app/routes/initiate-oauth.jsx
@@ -33,7 +33,7 @@ export const loader = async ({ request }) => {
 
   try {
     console.log("\n GET REQ -> Redirecting user to Instagram OAuth \n");
-    return redirect("https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=488937830712253&=https://admin.shopify.com/store/citsapptesting/apps/cits-instafeed-1/user-oauth&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish");
+    return redirect("https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=488937830712253&=https://admin.shopify.com/store/citsapptesting/apps/cits-instafeed-1/user-oauth&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish", { target: '_parent' });
   } catch (error) {
     console.error(error);
     return json({ success: false, error: error.message, editing });
