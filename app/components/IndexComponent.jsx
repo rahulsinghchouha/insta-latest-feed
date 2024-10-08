d39ef849393d071d42156f48ee97f48398e6917f
Rahul Singh Chouhan <134701207+rahulsinghchouha@users.noreply.github.com>
2024-10-08 13:25:00 +0530

    Update IndexComponent.jsx

--- app/components/IndexComponent.jsx
+++ app/components/IndexComponent.jsx
@@ -85,7 +85,7 @@ function IndexComponent({
 										<Link
 											removeUnderline
 											target="_blank"
-											url={`https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=488937830712253&redirect_uri=https://instacarousel24.onrender.com/auth/instagram/callback&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish&state={${queryP.sessionQ}}`}
+											url={`https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=488937830712253&redirect_uri=https://instacarousel24.onrender.com/auth/instagram/callback&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish&state=${queryP.sessionQ}`}
 										>
 											Link Your Instagram Account
 										</Link>
